14a425b0f316440b99546aa5d53b201dde7176b1
Sagar <sgrsngh2003@gmail.com>
2024-11-20 18:34:57 +0530

    k

--- Server/controllers/user.controller.js
+++ Server/controllers/user.controller.js
@@ -37,6 +37,7 @@ export const updateUser = async (req, res, next) => {
             req.params.id,
             {
                 $set: {
+                    name: req.body.name,
                     username: req.body.username,
                     password: req.body.password,
                     avatar: req.body.avatar,
